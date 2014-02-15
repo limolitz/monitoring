f8587c94989a19c3e947099cf8387580e7710a0c
Florin Godard <irgend@wasmitnetzen.de>
2014-02-15 03:13:55 +0100

    Renamed tables

--- Firefox/createLocalDb.py
+++ Firefox/createLocalDb.py
@@ -10,5 +10,6 @@ dbConnection = sql.connect('firefoxProcesses.db')
 with dbConnection:
         dbCursor = dbConnection.cursor()
         if args.dropDB:
-                dbCursor.execute("DROP TABLE IF EXISTS firefox")
-        dbCursor.execute("CREATE TABLE firefox (date INT PRIMARY KEY, profile TEXT, tabs INT)")
+                dbCursor.execute("DROP TABLE IF EXISTS tabNumber, focusedSite")
+        dbCursor.execute("CREATE TABLE focusedSite (id INT PRIMARY KEY, profile TEXT, host INT, firstVisit INT, time INT)")
+        dbCursor.execute("CREATE TABLE tabNumber (date INT PRIMARY KEY, profile TEXT, tabs INT)")
