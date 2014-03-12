3eb617f194e182a2c2bf1ee1a9d9aaf4b1803727
Florin Godard <irgend@wasmitnetzen.de>
2014-03-12 06:48:02 +0100

    Box width is now uptime, added legend and transmitted traffic

--- Traffic/plot.py
+++ Traffic/plot.py
@@ -11,12 +11,8 @@ def plot():
 	try:
 		con = sql.connect(getPathToDB())
 		cur = con.cursor()
-		cur.execute('SELECT date, trafficRX, trafficTX FROM traffic');
+		cur.execute('SELECT date, trafficRX, trafficTX, uptime FROM traffic');
 		data = cur.fetchall();
-
-		# TODO(wasmitnetzen): map all entries from same date to one entry
-		#print map(lambda x: [x[0], (x[1]/1024)], data);
-		#print data
 
 		g = Gnuplot.Gnuplot(persist=1)
 		g('set term png truecolor size 1000,500 font "Helvetica, 13pt" ')
@@ -30,21 +26,25 @@ def plot():
 
 
 		g('set xdata time')
+		# parse timestamp
 		g('set timefmt "%s"')
-		g('set format x "%d.%m"')
-		g('set xtic '+str(60*60*24*1))
+		# set ad day-month
+		g('set format x "%d.%m %H:%M"')
+		# tic with 1 day
+		g('set xtic '+str(60*60*24*0.5))
 		g('set xlabel "Date"')
 		g('set autoscale x')
 
+		# tic width 1 GiB
 		g('set ytic 1024')
 		g('set ylabel "Traffic (MiB)"')
 		g('set yrange [0:]')
 
-		plot = Gnuplot.PlotItems.Data(data, using="1:($2/1024**2)")
+		# Plot bytes in MiB (divide by 1024^2)
+		plot1 = Gnuplot.PlotItems.Data(data, using="1:($2/1024**2):4", title="Received")
+		plot2 = Gnuplot.PlotItems.Data(data, using="1:(($2+$3)/1024**2):4", title="Transmitted")
 
-		g.plot(plot)
-		#raw_input('Please press return to continue...\n')
-		#gplt.output(plotfile,'png medium transparent picsize 600 400')
+		g.plot(plot2, plot1)
 
 	except sql.Error, e:
 		print "Error %s:" % e.args[0]
