cdcb17b0b495ba26e51aea76bb1ba2da2bbc9feb
wasMitNetzen <irgend@wasmitnetzen.de>
2014-03-16 01:24:38 +0100

    Added overall traffic count, set bars at correct point in time

--- Traffic/plot.py
+++ Traffic/plot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sqlite3 as sql
 import datetime
 import Gnuplot
@@ -11,11 +12,24 @@ def plot():
 	try:
 		con = sql.connect(getPathToDB())
 		cur = con.cursor()
-		cur.execute('SELECT date, trafficRX, trafficTX, uptime FROM traffic');
+		cur.execute('SELECT date, trafficRX, trafficTX, uptime FROM traffic ORDER BY date');
 		data = cur.fetchall();
 
+		# build overall traffic counter
+		counter = 0;
+		overallTraffic = [];
+		for index in range(len(data)):
+			counter += data[index][1]+data[index][2];
+			overallTraffic.append((data[index][0]+data[index][3]/2, counter));
+		#print data[1]
+		#print datetime.datetime.utcfromtimestamp(data[0][0]);
+		#delta = datetime.timedelta(0, int(data[0][3]));
+		#print delta;
+		#print datetime.datetime.utcfromtimestamp(data[0][0])+delta;
+
+		#print overallTraffic;
 		g = Gnuplot.Gnuplot(persist=1)
-		g('set term png truecolor size 1000,500 font "Helvetica, 13pt" ')
+		g('set term png truecolor size 1000,750 font "Helvetica, 13pt" ')
 		g('set output "traffic.png"')
 		g('set title "Traffic per run"')
 		g('set grid')
@@ -28,22 +42,31 @@ def plot():
 		# parse timestamp
 		g('set timefmt "%s"')
 		# set ad day-month
-		g('set format x "%d.%m %H:%M"')
+		g('set format x "%d.%m."')
+		#g('set format x "%s"')
 		# tic with 1 day
-		g('set xtic '+str(60*60*24*0.5))
-		g('set xlabel "Date"')
+		g('set xtic '+str(60*60*24*1))
+		g('set xlabel "Date (UTC)"')
 		g('set autoscale x')
 
-		# tic width 1 GiB
-		g('set ytic 1024')
-		g('set ylabel "Traffic (MiB)"')
+		# tic width 4 GiB
+		g('set ytic 4')
+		g('set ylabel "Traffic per run (GiB)"')
 		g('set yrange [0:]')
 
-		# Plot bytes in MiB (divide by 1024^2)
-		plot1 = Gnuplot.PlotItems.Data(data, using="1:($2/1024**2):4", title="Received")
-		plot2 = Gnuplot.PlotItems.Data(data, using="1:(($2+$3)/1024**2):4", title="Transmitted")
+		g('set y2tic 16')
+		g('set autoscale y2')
+		g('set y2label "Overall Traffic (GiB)"')
+		g('set y2range [0:]')
 
-		g.plot(plot2, plot1)
+		g('set key center bottom outside horizontal')
+
+		# Plot bytes in MiB (divide by 1024^2)
+		plot1 = Gnuplot.PlotItems.Data(data, using="($1+($4/2)):($2/1024**3):4", title="Received")
+		plot2 = Gnuplot.PlotItems.Data(data, using="($1+($4/2)):(($2+$3)/1024**3):4", title="Transmitted")
+		plot3 = Gnuplot.PlotItems.Data(overallTraffic, title="Overall Traffic", with_="steps", using="1:($2/1024**3)", axes="x1y2")
+
+		g.plot(plot2, plot1, plot3)
 
 	except sql.Error, e:
 		print "Error %s:" % e.args[0]
