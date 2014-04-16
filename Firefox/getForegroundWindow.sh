b84cf9b5788f4a3be1a76ded085c83f21f113f7f
wasMitNetzen <irgend@wasmitnetzen.de>
2014-04-16 20:46:34 +0200

    Fixed bug on calling with cronjob, should work now

--- Firefox/getForegroundWindow.sh
+++ Firefox/getForegroundWindow.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-windowID=$(xprop -root|grep ^_NET_ACTIVE_WINDOW | grep -o "0x[0-9a-f]*")
+windowID=$(/usr/bin/xprop -display :0 -root _NET_ACTIVE_WINDOW | /bin/grep -oP "0x[0-9a-f]{2,}")
 #echo "WindowID: $windowID"
-procID=$(xprop -id $windowID | grep "_NET_WM_PID" | grep -o "[0-9]*$")
+procID=$(/usr/bin/xprop -display :0 -id $windowID | /bin/grep "_NET_WM_PID" | /bin/grep -o "[0-9]*$")
 #echo "ProcID: $procID"
 #echo -n "Commandline: "
 cat /proc/$procID/cmdline
