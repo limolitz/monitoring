1468b1708bb224ab923616610f57acd59e3d0eae
wasMitNetzen <irgend@wasmitnetzen.de>
2014-06-21 01:46:00 +0200

    Fix bug with timezones

--- CPU/cron.sh
+++ CPU/cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/florin/bin/QuantifiedSelf/CPU
-python loadAverages.py
-gnuplot loadAverages.plot
+TZ=UTC python loadAverages.py
+TZ=CEST gnuplot loadAverages.plot
 scp cpu.png leo:/home/florin/html/home2/images/cpu_kiwi.png
