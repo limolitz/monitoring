53936fc9e99c5b1fb9c85a2495ae3ba38c41788d
Florin Godard <fgodard@gmail.com>
2014-11-02 21:36:27 +0000

    Added language independency

--- CPU/loadAverages.sh
+++ CPU/loadAverages.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/usr/bin/uptime | /bin/grep -oP "[0-9],[0-9]{2}, [0-9],[0-9]{2}, [0-9],[0-9]{2}"
+LANG=ALL /usr/bin/uptime | /bin/grep -oP "[0-9].[0-9]{2}, [0-9].[0-9]{2}, [0-9].[0-9]{2}"
