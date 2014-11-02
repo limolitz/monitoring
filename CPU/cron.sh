ef9eba1d0afea34cbdee2ca177167e8eb8703379
wasMitNetzen <irgend@wasmitnetzen.de>
2014-11-02 22:43:16 +0100

    Added uname to file name

--- CPU/cron.sh
+++ CPU/cron.sh
@@ -2,4 +2,7 @@
 cd /home/florin/bin/QuantifiedSelf/CPU
 TZ=UTC python loadAverages.py
 TZ=UTC gnuplot loadAverages.plot
-scp cpu.png leo:/home/florin/html/home2/images/cpu_kiwi.png
+mv cpu.png cpu-$(uname -n).png
+ssh leo true
+scp cpu-$(uname -n).png leo:/home/florin/html/home2/images/
+
