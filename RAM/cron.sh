477496ab3a4d1aa148054b58ba31f49b4adbae18
wasMitNetzen <irgend@wasmitnetzen.de>
2014-11-02 22:48:36 +0100

    Added uname to file

--- RAM/cron.sh
+++ RAM/cron.sh
@@ -2,4 +2,6 @@
 cd /home/florin/bin/QuantifiedSelf/RAM
 TZ=UTC python ramUsage.py
 TZ=UTC gnuplot ramUsage.plot
-scp ram.png leo:/home/florin/html/home2/images/ram_kiwi.png
+ssh leo true
+mv ram.png ram-$(uname -n).png
+scp ram-$(uname -n).png leo:/home/florin/html/home2/images/
