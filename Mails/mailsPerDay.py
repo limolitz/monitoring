86caddf095c47f157dcc6c48eddbeef21e2fe4cf
wasMitNetzen <irgend@wasmitnetzen.de>
2015-05-10 21:49:11 +0200

    Renamed script for stats per day, renamed output file

--- Mails/mailsPerDay.py
+++ Mails/mailsPerDay.py
@@ -56,52 +56,56 @@ def countReceived(day):
 	return receivedMails
 
 def plot(data):
-		g = Gnuplot.Gnuplot(persist=1)
-		g('set term png truecolor size 1000,500 font "Helvetica, 13pt" ')
-		g('set title "Mails Received And Sent In '+data[0][0].strftime('%B %Y')+'"')
-		g('set grid')
-		g('set grid mxtics')
-		filename = "mails_" + str(data[0][0].month) + str(data[0][0].year)+ ".png"
-		g('set output "'+filename+'"')
+	g = Gnuplot.Gnuplot(persist=1)
+	g('set term png truecolor size 1000,500 font "Helvetica, 13pt" ')
+	g('set title "Mails Received And Sent In '+data[0][0].strftime('%B %Y')+'"')
+	g('set grid')
+	g('set grid mxtics')
+	filename = "mailsPerDay_" + str(data[0][0].month) + str(data[0][0].year)+ ".png"
+	g('set output "'+filename+'"')
 
-		g('set xdata time')
+	g('set xdata time')
 
-		# parse timestamp
-		g('set timefmt "%s"')
+	# parse timestamp
+	g('set timefmt "%s"')
 
-		# set ad day-month
-		g('set format x "%d.%m."')
+	# set ad day-month
+	g('set format x "%d.%m."')
 
-		# tic with 1 day
-		g('set xtic '+str(60*60*24*1))
-		g('set xlabel "Date"')
-		g('set autoscale x')
+	# tic with 1 day
+	g('set xtic '+str(60*60*24*1))
+	g('set xlabel "Date"')
+	g('set autoscale x')
 
-		g('set ytic 5')
-		g('set ylabel "# of Mails"')
-		g('set yrange [0:]')
+	g('set ytic 5')
+	g('set ylabel "# of Mails"')
+	g('set yrange [0:]')
 
-		#g('set y2tic 4')
-		#g('set autoscale y2')
-		#g('set y2label "Received Mails"')
-		#g('set y2range [0:]')
+	#g('set y2tic 4')
+	#g('set autoscale y2')
+	#g('set y2label "Received Mails"')
+	#g('set y2range [0:]')
 
-		g('set key center bottom outside horizontal')
+	g('set key center bottom outside horizontal')
 
-		g('set style fill solid 1.0')
-		g('set style data boxes')
+	g('set style fill solid 1.0')
+	g('set style data boxes')
 
-		formatted = []
-		for elem in data:
-			formatted.append([elem[1], elem[2], elem[3]])
+	formatted = []
+	for elem in data:
+		formatted.append([elem[1], elem[2], elem[3]])
 
-		plot1 = Gnuplot.PlotItems.Data(formatted, using="($1-60*60*5):($2):(60*60*8)", title="Sent")
-		plot2 = Gnuplot.PlotItems.Data(formatted, using="($1+60*60*5):($3):(60*60*8)", title="Received")
+	plot1 = Gnuplot.PlotItems.Data(formatted, using="($1-60*60*5):($2):(60*60*8)", title="Sent")
+	plot2 = Gnuplot.PlotItems.Data(formatted, using="($1+60*60*5):($3):(60*60*8)", title="Received")
 
-		g.plot(plot1, plot2)
+	g.plot(plot1, plot2)
 
 
 if __name__ == '__main__':
+	# read params
+	if len(sys.argv) > 1 or len(sys.argv) < 1:
+		print "Usage: python mails.py"
+		exit()
 	login();
 	data = []
 	for day in range(1, date.today().day):
