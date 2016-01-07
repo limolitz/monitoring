3334d708d74f68352cd3f9bee0d77ff98566448d
Florin Godard <fgodard@gmail.com>
2016-01-07 18:01:07 +0000

    Removed debug, thereby fixing bug with mails without subject

--- Mails/mailsInboxCount.py
+++ Mails/mailsInboxCount.py
@@ -21,32 +21,28 @@ def login():
 
 def countFolder(folder):
 	global imap
-	
+
 	imap.select(folder, True);
 	rv, search = imap.search(None, 'ALL');
-	print (search);
 	folderCount = len(search[0].split());
-	
+
 	timestamps = []
-	# based on https://gist.github.com/robulouski/7441883	
+	# based on https://gist.github.com/robulouski/7441883
 	for num in search[0].split():
 		rv, data = imap.fetch(num, '(RFC822)')
 		if rv != 'OK':
 			print("ERROR getting message", num)
 			return
-		
+
 		msg = email.message_from_bytes(data[0][1])
-		hdr = email.header.make_header(email.header.decode_header(msg['Subject']))
-		subject = str(hdr)
-		#print('Message %s: %s' % (num, subject))
-		#print('Raw Date:', msg['Date'])
+
 		# Now convert to local date-time
 		date_tuple = email.utils.parsedate_tz(msg['Date'])
 		if date_tuple:
 			timestamp = email.utils.mktime_tz(date_tuple)
 			#print ("Timestamp:", timestamp)
 			timestamps.append(timestamp)
-	
+
 
 	return (folderCount, timestamps);
 
@@ -54,14 +50,14 @@ def getDataForToday():
 	inboxCount, inboxTimestamps = countFolder('INBOX');
 	uniCount, uniTimestamps = countFolder('Uni');
 	#print (inboxTimestamps, mean(inboxTimestamps), );
-	
+
 	return (str(inboxCount), str(uniCount), int(datetime.datetime.utcnow().strftime("%s"))-(sum(inboxTimestamps) / float(len(inboxTimestamps))), int(datetime.datetime.utcnow().strftime("%s"))-(sum(uniTimestamps) / float(len(uniTimestamps))));
 
 def storeData():
 	data = getDataForToday();
 	with open("/home/florin/bin/QuantifiedSelf/Mails/inbox.data", "a") as myfile:
 		myfile.write(datetime.datetime.utcnow().strftime("%s")+"	"+data[0]+"	"+data[1]+"	"+str(data[2])+"	"+str(data[3])+"\n");
-		
+
 def getData():
 	with open("/home/florin/bin/QuantifiedSelf/Mails/inbox.data") as f:
 		content = f.readlines()
@@ -71,7 +67,7 @@ def getData():
 		items = item.rstrip().split("\t")
 		#print(items)
 		data.append(items)
-	
+
 	return data;
 
 if __name__ == '__main__':
