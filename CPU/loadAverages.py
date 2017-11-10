9e8977130887561e42299a9b7aa3807bd6cd3716
Florin Godard <fgodard@gmail.com>
2017-11-10 21:52:14 +0000

    Add MQTT sending

--- CPU/loadAverages.py
+++ CPU/loadAverages.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
 import datetime
+import json
 
 # read new data
 loadAverages = subprocess.Popen('/home/florin/bin/QuantifiedSelf/CPU/loadAverages.sh', stdout=subprocess.PIPE).stdout.read()
 averages = []
-print(datetime.datetime.utcnow())
+print("Date: {}".format(datetime.datetime.utcnow()))
 averages.append(datetime.datetime.utcnow().strftime("%s"))
 for avg in str(loadAverages).rstrip().split(", "):
 	averages.append(avg)
@@ -33,3 +34,18 @@ content.append(averages)
 with open("/home/florin/bin/QuantifiedSelf/CPU/cpu.data", "w") as myfile:
 	for avg in data:
 		myfile.write(avg[0]+"	"+avg[1]+"	"+avg[2]+"	"+avg[3]+"\n")
+
+# write object for MQTT sending
+mqttObject = {
+	"topic": "cpu",
+	"measurements": {
+		"avg1": avg[1],
+		"avg5": avg[2],
+		"avg15": avg[3]
+	}
+}
+
+json = json.dumps(mqttObject)
+print("Writing JSON: {}".format(json))
+sender = subprocess.Popen(["/home/florin/bin/mqttsend/mqttsend.sh"], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+sender.stdin.write(json)
