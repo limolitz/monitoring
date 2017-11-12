100efb1bfad8bc550ccc754ff540094af7547b69
Florin Godard <fgodard@gmail.com>
2017-11-12 21:56:16 +0000

    Adjusted RAM scripts from local storage to pure MQTT sending of data

--- RAM/cron.sh
+++ RAM/cron.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 cd /home/florin/bin/QuantifiedSelf/RAM
-TZ=UTC python ramUsage.py
-TZ=UTC gnuplot ramUsage.plot
-ssh leo true
-mv ram.png ram-$(uname -n).png
-scp ram-$(uname -n).png leo:/home/florin/html/home2/images/
+TZ=UTC /usr/local/bin/python3.6 ramUsage.py >> cron.log
--- RAM/ramUsage.py
+++ RAM/ramUsage.py
@@ -1,58 +1,43 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python3.6
 import subprocess
 import datetime
+import configparser
+import json
 
 def getRAMofProcess(processName):
 	proc = subprocess.Popen('ps -C '+processName+' -o rss=', stdout=subprocess.PIPE, shell=True)
-	print(memTotal)
 	sums = 0
 	for line in proc.stdout:
-		#the real code does filtering here
 		sums += int(line.rstrip())
-	#print(sums)
 	return sums
-   
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+watchedProgramsPlain = config.get("Programs", "interesting")
+watchedPrograms = watchedProgramsPlain.split(',')
+#print("Watched programs: {}".format(watchedPrograms))
 
 # read new data
 memTotal = int(subprocess.Popen('cat /proc/meminfo | grep -o "MemTotal.*" | grep -o "[0-9]*"', stdout=subprocess.PIPE, shell=True).stdout.read())
 memFree = int(subprocess.Popen('cat /proc/meminfo | grep -o "MemFree.*" | grep -o "[0-9]*"', stdout=subprocess.PIPE, shell=True).stdout.read())
 memUsed = memTotal - memFree
 
-# processes
-firefox = getRAMofProcess('firefox')
-chrome = getRAMofProcess('chrome')
-print("Firefox: "+str(firefox))
-print("Chrome: "+str(chrome))
-print('Date: '+str(datetime.datetime.utcnow()))
-memValues = []
-memValues.append(datetime.datetime.utcnow().strftime("%s"))
-memValues.append(memTotal)
-memValues.append(memFree)
-memValues.append(memUsed)
-memValues.append(firefox)
-memValues.append(chrome)
-#print(memUsed)
-print(memValues)
 
-# load old data
-with open("/home/florin/bin/QuantifiedSelf/RAM/ram.data") as f:
-	content = f.readlines()
+memoryInfo = {
+	"total": memTotal,
+	"free": memFree,
+	"used": memUsed
+}
+for programName in watchedPrograms:
+	memoryInfo[programName] = getRAMofProcess(programName)
 
-data = []
-for item in content:
-	items = item.rstrip().split("\t")
-	#print(items)
-	data.append(items)
 
-data.append(memValues)
+mqttObject = {
+	"topic": "ram",
+	"measurements": memoryInfo
+}
 
-# discard old data
-data = data[-60:]
-
-# append new data
-content.append(memValues)
-
-#print(content)
-with open("/home/florin/bin/QuantifiedSelf/RAM/ram.data", "w") as myfile:
-	for value in data:
-		myfile.write(str(value[0])+"	"+str(value[1])+"	"+str(value[2])+"	"+str(value[3])+"	"+str(value[4])+"	"+str(value[5])+"\n")
+json = json.dumps(mqttObject)
+print("Writing JSON: {}".format(json))
+sender = subprocess.Popen([config.get("Paths", "mqttPath")], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+sender.stdin.write(json.encode('utf-8'))
