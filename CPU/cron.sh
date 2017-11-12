690ff85457181ac7d7ffdc12f846c7b0a8119e9c
Florin Godard <fgodard@gmail.com>
2017-11-12 22:11:34 +0000

    Move CPU logging to pure MQTT storage, remove local storage

--- CPU/cron.sh
+++ CPU/cron.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 cd /home/florin/bin/QuantifiedSelf/CPU
 TZ=UTC python loadAverages.py
-TZ=UTC gnuplot loadAverages.plot
-mv cpu.png cpu-$(uname -n).png
-ssh leo true
-scp cpu-$(uname -n).png leo:/home/florin/html/home2/images/
-
--- CPU/loadAverages.py
+++ CPU/loadAverages.py
@@ -6,42 +6,16 @@ import json
 # read new data
 loadAverages = subprocess.Popen('/home/florin/bin/QuantifiedSelf/CPU/loadAverages.sh', stdout=subprocess.PIPE).stdout.read()
 averages = []
-print("Date: {}".format(datetime.datetime.utcnow()))
-averages.append(datetime.datetime.utcnow().strftime("%s"))
 for avg in str(loadAverages).rstrip().split(", "):
 	averages.append(avg)
-#print(averages)
-
-# load old data
-with open("/home/florin/bin/QuantifiedSelf/CPU/cpu.data") as f:
-	content = f.readlines()
-
-data = []
-for item in content:
-	items = item.rstrip().split("\t")
-	#print(items)
-	data.append(items)
-
-data.append(averages)
-
-# discard old data
-data = data[-60:]
-
-# append new data
-content.append(averages)
-
-#print(content)
-with open("/home/florin/bin/QuantifiedSelf/CPU/cpu.data", "w") as myfile:
-	for avg in data:
-		myfile.write(avg[0]+"	"+avg[1]+"	"+avg[2]+"	"+avg[3]+"\n")
 
 # write object for MQTT sending
 mqttObject = {
 	"topic": "cpu",
 	"measurements": {
-		"avg1": avg[1],
-		"avg5": avg[2],
-		"avg15": avg[3]
+		"avg1": averages[0],
+		"avg5": averages[1],
+		"avg15": averages[2]
 	}
 }
 
