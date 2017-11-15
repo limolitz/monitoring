f5e1c56d74e44d661cc861b99e53f4e00e7884b4
Florin Godard <florin@wasmitnetzen.de>
2017-11-15 17:09:07 +0100

    Add MacOS support, introduce config file for MQTT script location

--- CPU/cron.sh
+++ CPU/cron.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 cd $(dirname "${BASH_SOURCE[0]}")
-TZ=UTC python loadAverages.py
+
+if [ "$(uname -s)" = "Darwin" ]; then
+	export PATH=/usr/local/opt/python3/bin/:$PATH
+fi
+TZ=UTC python3 loadAverages.py
