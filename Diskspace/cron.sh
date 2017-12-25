da6ddc2c428798c8dee34c31a1ea3bb8b963a7c9
Florin Godard <irgend@wasmitnetzen.de>
2017-12-25 18:26:13 +0100

    Support symlink to cron.sh for diskspace

--- Diskspace/cron.sh
+++ Diskspace/cron.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cd $(dirname "${BASH_SOURCE[0]}")
+# check if filename is a symlink
+if [[ -L "${BASH_SOURCE[0]}" ]]; then
+	cd $(dirname $(readlink "${BASH_SOURCE[0]}"))
+else
+	cd $(dirname "${BASH_SOURCE[0]}")
+fi
 
 if [ "$(uname -s)" = "Darwin" ]; then
 	export PATH=/usr/local/opt/python3/bin/:$PATH
