f0355faf839ca12707d7267733e9a9a2592d0eee
Florin Godard <florin@wasmitnetzen.de>
2018-07-12 12:45:18 +0000

    Always use POSIX formatting, no matter the locale

--- CPU/cron.sh
+++ CPU/cron.sh
@@ -4,4 +4,4 @@ cd $(dirname "${BASH_SOURCE[0]}")
 if [ "$(uname -s)" = "Darwin" ]; then
 	export PATH=/usr/local/opt/python3/bin/:$PATH
 fi
-TZ=UTC python3 loadAverages.py
+LC_ALL=POSIX TZ=UTC python3 loadAverages.py
