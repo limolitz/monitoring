5fb9af1cb093550e075e8e9388451fbd253644e3
wasMitNetzen <irgend@wasmitnetzen.de>
2020-11-10 23:09:16 +0100

    Add ip parsing when it doesn't support -json yet

--- Traffic/countTraffic.py
+++ Traffic/countTraffic.py
@@ -9,11 +9,30 @@ import json
 import platform
 
 def getAllInterfaces():
-	ip = subprocess.Popen(["ip", "-json", "link", "show"],stdout=subprocess.PIPE)
+	ip = subprocess.Popen(["ip", "-json", "link", "show"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 	output, errors = ip.communicate()
-	data = json.loads(output.decode("utf-8"))
-	names = [entry["ifname"] for entry in data]
-	return names
+	# no errors
+	if len(errors) == 0:
+		data = json.loads(output.decode("utf-8"))
+		names = [entry["ifname"] for entry in data]
+		return names
+	else:
+		# check if ip just doesn't support -json yet
+		if "Option \"-json\" is unknown" in errors.decode("utf-8"):
+			# fallback to plain output parsing
+			# piping to grep
+			# https://stackoverflow.com/questions/13332268/how-to-use-subprocess-command-with-pipes
+			# grep output groups
+			# https://unix.stackexchange.com/questions/13466/can-grep-output-only-specified-groupings-that-match
+			ip = subprocess.Popen(["ip", "link" ,"show"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			grep = subprocess.Popen(["grep", "-oP", "^[0-9]*: (\\K[a-z0-9]*)"], stdin=ip.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+			ip.stdout.close()
+			output, errors = grep.communicate()
+			if errors:
+				print(errors, file=sys.stderr)
+			return output.decode("utf-8").splitlines()
+		else:
+			print("Error on ip call: {}".format(errors.decode("utf-8")), file=sys.stderr)
 
 def trafficInBytes(interface):
 	if platform.system() == "Darwin":
@@ -55,6 +74,9 @@ if __name__ == '__main__':
 	selfPath = config.get("Paths", "selfPath")
 
 	interfaces = getAllInterfaces()
+	if interfaces is None:
+		print("Error on getAllInterfaces.", file=sys.stderr)
+		exit(1)
 	for interface in interfaces:
 
 		uptime = uptimeInSeconds(selfPath)
@@ -72,4 +94,6 @@ if __name__ == '__main__':
 		print("Writing JSON: {}".format(jsonObject))
 		sender = subprocess.Popen([config.get("Paths", "mqttPath")], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
 		output, errors = sender.communicate(jsonObject.encode("utf-8"))
-		print(output.decode("utf-8"),errors.decode("utf-8"))
+		print(output.decode("utf-8"))
+		if errors:
+			print(errors.decode("utf-8"), file=sys.stderr)
