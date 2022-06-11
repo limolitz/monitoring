910e6a61fb0c0cd7bc8489b4d346fab05281efc0
Florin Godard <irgend-git@wasmitnetzen.de>
2022-06-11 20:40:03 +0100

    Refactor, use /proc/loadavg directly, use logger

--- CPU/loadAverages.py
+++ CPU/loadAverages.py
@@ -1,31 +1,41 @@
 #!/usr/bin/env python3
+
 import subprocess
-import datetime
 import json
 import configparser
+import logging
+import logging.config
+
 
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+logging.config.fileConfig(config)
+
+_logger = logging.getLogger(__name__)
+
 # read new data
-loadAveragesProcess = subprocess.Popen('./loadAverages.sh', stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-loadAverages, errors = loadAveragesProcess.communicate()
-averages = []
-for avg in loadAverages.decode("utf-8").rstrip().split(", "):
-	averages.append(avg)
+with open('/proc/loadavg') as file:
+    data = file.read()
+
+averages = data.split(" ")[:3]
 
 # write object for MQTT sending
 mqttObject = {
-	"topic": "cpu",
-	"measurements": {
-		"avg1": averages[0],
-		"avg5": averages[1],
-		"avg15": averages[2]
-	}
+    "topic": "cpu",
+    "measurements": {
+        "avg1": averages[0],
+        "avg5": averages[1],
+        "avg15": averages[2]
+    }
 }
 
 json = json.dumps(mqttObject)
-print("Writing JSON: {}".format(json))
-sender = subprocess.Popen([config.get("Paths", "mqttPath")], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+_logger.debug("Writing JSON: {}".format(json))
+sender = subprocess.Popen(
+    [config.get("Paths", "mqttPath")],
+    stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE
+)
 output, errors = sender.communicate(json.encode("utf-8"))
-print(output.decode("utf-8"),errors.decode("utf-8"))
+_logger.debug(output.decode("utf-8"))
+_logger.error(errors.decode("utf-8"))
